22666318425329699abdf89a3feec2a328fab992
AsenPlesnev <as.plesnev@gmail.com>
2020-08-14 16:05:49 +0300

    Added HomePage component

---
+++ notebook/src/components/HomePage/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import styles from './index.module.css';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className={styles['gradient-animation']} style={{ "overflowX": "hidden" }}>
+                <div className={"row"}>
+                    <div className={"col s12"}>
+                        <div className={styles.valign}>
+                            <div>
+                                <h1 className={"header center white-text"}>
+                                    <span className={"red-text text-lighten-3"}>Net</span>Book
+                                </h1>
+                                <p className={"flow-text center white-text"}>Проверка на ученик</p>
+                                <form action="#">
+                                    <div className={"container"}>
+                                        <div className={"row"}>
+                                            <div className={"input-field col s12 m4 push-m4"}>
+                                                <label htmlFor="code">Код</label>
+                                                <input id="code" name="code" type="text"></input>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p className={"center"}>
+                                        <button className={`${styles["button-total"]} ${styles["button-3e"]} button-3 icon-arrow-right red lighten-3 white-text`}>Напред</button>
+                                    </p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
